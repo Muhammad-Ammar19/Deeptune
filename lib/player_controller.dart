37263db63352740f642d5e6d9795073e50620c4a
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-04-24 19:55:36 +0500

    .

--- lib/player_controller.dart
+++ lib/player_controller.dart
@@ -1,11 +1,7 @@
-
 import 'package:get/get.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
-
-
-
 
 
 class PlayerController extends GetxController {
