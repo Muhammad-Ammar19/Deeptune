6d9bbe71d9bde0a3c95f79a2ee32057ea00b0802
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-04-29 20:19:47 +0500

    .

--- lib/bottom_player.dart
+++ lib/bottom_player.dart
@@ -1,37 +1,51 @@
 import 'package:deeptune_musicplayer/player_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-
+import 'package:on_audio_query/on_audio_query.dart';
 
 class BottomMusicPlayer extends StatelessWidget {
-  
-    const BottomMusicPlayer({super.key});
-  
+  const BottomMusicPlayer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var controller = Get.find<PlayerController>();                               // Access PlayerController
-  
+    var controller = Get.find<PlayerController>();         // Access PlayerController
+
     return Obx(() {
       var currentSong = controller.selectedSong.value;
       var isPlaying = controller.isPlaying.value;
 
       if (currentSong == null) {
-        return const Card();                                                     // Placeholder widget when no song is selected
+        return const Card();                               // Placeholder widget when no song is selected
       }
 
-      return GestureDetector( onTap: () {
-    
-      },
-        child: Card(elevation: 4,
+      return GestureDetector(
+        onTap: () {},
+        child: Card(
+          elevation: 4,
           child: Container(
-            height: Get.height *0.1,
+            height: Get.height * 0.1,
             width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 16),
+            padding: const EdgeInsets.only(right: 10),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [ 
+              children: [
+                Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  elevation: 5,
+                  child: QueryArtworkWidget(
+                    id: 6,
+                    type: ArtworkType.AUDIO,
+                    artworkHeight: Get.height * 0.01,
+                    artworkFit: BoxFit.cover,
+                    artworkQuality: FilterQuality.high,
+                    artworkBorder: BorderRadius.circular(6),
+                    artworkWidth: double.infinity,
+                    quality: 100,
+                    nullArtworkWidget: const Icon(
+                      Icons.music_note_rounded,
+                      size: 50,
+                    ),
+                  ),
+                ),
                 // Song Info and Controls
                 Expanded(
                   child: Column(
@@ -50,9 +64,6 @@ class BottomMusicPlayer extends StatelessWidget {
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                       ),
-                 
-                  
-                  
                     ],
                   ),
                 ),
@@ -75,40 +86,3 @@ class BottomMusicPlayer extends StatelessWidget {
     });
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
