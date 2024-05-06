d88afc4fa9ff2af37412db64a16f823aac60bd19
Muhammad Ammar <syedammarzaki4@gmail.com>
2024-05-06 20:21:40 +0500

    .

--- lib/all_songs_page.dart
+++ lib/all_songs_page.dart
@@ -12,88 +12,90 @@ class AllSongsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var controller = Get.put(PlayerController());
 
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          GestureDetector(
-              onTap: () {
-                Get.to(() => SearchList(), transition: Transition.fadeIn);
-              },
-              child: const Padding(
-                padding: EdgeInsets.only(right: 18),
-                child: Icon(
-                  Icons.search,
-                  size: 25,
-                ),
-              ))
-        ],
-        centerTitle: true,
-        title: const Text(
-          "All Songs",
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            GestureDetector(
+                onTap: () {
+                  Get.to(() => SearchList(), transition: Transition.fadeIn);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.only(right: 18),
+                  child: Icon(
+                    Icons.search,
+                    size: 25,
+                  ),
+                ))
+          ],
+          centerTitle: true,
+          title: const Text(
+            "All Songs",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-      body: FutureBuilder<List<SongModel>>(
-        future: controller.audioquery.querySongs(
-            ignoreCase: true,
-            orderType: OrderType.ASC_OR_SMALLER,
-            sortType: null,
-            uriType: UriType.EXTERNAL),
-        builder: (BuildContext context, snapshot) {
-          if (snapshot.data == null) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.data!.isEmpty) {
-            return const Center(
-              child: Text("Snap,No Songs were found"),
-            );
-          } else {
-            return ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: snapshot.data!.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: Card(
-                      elevation: 4,
-                      child: QueryArtworkWidget(
-                        id: snapshot.data![index].id,
-                        type: ArtworkType.AUDIO,
-                        nullArtworkWidget: const Icon(
-                          Icons.music_note_rounded,
-                          size: 50,
+        body: FutureBuilder<List<SongModel>>(
+          future: controller.audioquery.querySongs(
+              ignoreCase: true,
+              orderType: OrderType.ASC_OR_SMALLER,
+              sortType: null,
+              uriType: UriType.EXTERNAL),
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.data == null) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.data!.isEmpty) {
+              return const Center(
+                child: Text("Snap,No Songs were found"),
+              );
+            } else {
+              return ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: Card(
+                        elevation: 4,
+                        child: QueryArtworkWidget(
+                          id: snapshot.data![index].id,
+                          type: ArtworkType.AUDIO,
+                          nullArtworkWidget: const Icon(
+                            Icons.music_note_rounded,
+                            size: 50,
+                          ),
+                          artworkFit: BoxFit.cover,
+                          artworkBorder: BorderRadius.circular(4),
                         ),
-                        artworkFit: BoxFit.cover,
-                        artworkBorder: BorderRadius.circular(4),
                       ),
-                    ),
-                    title: Text(
-                      snapshot.data![index].displayNameWOExt,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w500, fontSize: 14),
-                    ),
-                    subtitle: Text(
-                      "${snapshot.data![index].artist}",
-                      style: const TextStyle(fontSize: 11),
-                    ),
-                    trailing: controller.playIndex.value == index &&
-                            controller.isPlaying.value
-                        ? const Icon(Icons.play_arrow_rounded)
-                        : null,
-                    onTap: () {
-                      controller.playSong(snapshot.data![index].uri, index);
-            
-                      Get.find<PlayerController>()
-                          .updateSelectedSong(snapshot.data![index]);
-                      Get.to(() => SongPage(data: snapshot.data!),
-                          transition: Transition.fadeIn);
-                    },
-                  );
-                });
-          }
-        },
+                      title: Text(
+                        snapshot.data![index].displayNameWOExt,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 14),
+                      ),
+                      subtitle: Text(
+                        "${snapshot.data![index].artist}",
+                        style: const TextStyle(fontSize: 11),
+                      ),
+                      trailing: controller.playIndex.value == index &&
+                              controller.isPlaying.value
+                          ? const Icon(Icons.play_arrow_rounded)
+                          : null,
+                      onTap: () {
+                        controller.playSong(snapshot.data![index].uri, index);
+
+                        Get.find<PlayerController>()
+                            .updateSelectedSong(snapshot.data![index]);
+                        Get.to(() => SongPage(data: snapshot.data!),
+                            transition: Transition.fadeIn);
+                      },
+                    );
+                  });
+            }
+          },
+        ),
       ),
     );
   }
